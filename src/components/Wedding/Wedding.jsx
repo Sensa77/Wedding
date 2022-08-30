b457c389a1843e279992031b5bfe4546ae78c114
Igor Abdulagadjiev <s89183906500@gmail.com>
2022-08-30 22:15:25 +0300

    fix wedding module

--- src/components/Wedding/Wedding.jsx
+++ src/components/Wedding/Wedding.jsx
@@ -10,29 +10,31 @@ export const Wedding = () => {
         <div className={styles.content}>
           <div className={styles.date}>
             <span className={`${styles.caption} ${styles.captionDate}`}>
-              Когда
+              Церемония
             </span>
             <p className={styles.description}>30 сентября 2022, 12:00</p>
-            <p className={styles.description}>30 сентября 2022, 18:00</p>
+            <a
+              href="https://yandex.ru/maps/-/CCUNmAVJ2B"
+              target="_blank"
+              className={styles.description}
+              rel="noreferrer"
+            >
+              <p className={styles.description}>ул.Стасова 174/1</p>
+            </a>
           </div>
           <img className={styles.picture} src={branch} />
           <div className={styles.location}>
             <span className={`${styles.caption} ${styles.captionLocation}`}>
-              Где
+              Ресторан
             </span>
-            <a
-              href="https://yandex.ru/maps/-/CCUNmAVJ2B"
-              target="_blank"
-              className={styles.description} rel="noreferrer"
-            >
-              Церемония - ул.Стасова 174/1
-            </a>
+            <p className={styles.description}>30 сентября 2022, 18:00</p>
             <a
               href="https://yandex.ru/maps/-/CCUNmAfmgC"
               target="_blank"
-              className={styles.description} rel="noreferrer"
+              className={styles.description}
+              rel="noreferrer"
             >
-              Ресторан - ул. Мачуги, д. 139
+              ул. Мачуги, д. 139
             </a>
           </div>
         </div>
